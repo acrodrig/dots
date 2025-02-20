f923d54c2c005b5fea7a795ac4e841620d7d9764
Andres C. Rodriguez <acrodrig@users.noreply.github.com>
2025-02-20 10:20:15 -0800

    Commenting coverage code in CI

--- scripts/release.ts
+++ scripts/release.ts
@@ -1,10 +1,20 @@
 #!/usr/bin/env -S deno run -A
 
+import { parseArgs } from "jsr:@std/cli/parse-args";
 import $ from "jsr:@david/dax";
 
+
+// Define usage message and style
+const USAGE = "Usage: release [-h|--help] [-e|--execute] [version]";
+
+// Parse command-line arguments
+const args = parseArgs(Deno.args, { boolean: ["h", "help", "e", "execute"], alias: { h: "help", e: "execute" }, unknown: (arg) => arg });
+if (args.help) console.log(USAGE), Deno.exit(0);
+
 // Test formatting
-let version = Deno.args[0];
-if (version && !version?.match(/^[0-9]+\.[0-9]+\.[0-9]+$/)) console.error("❌ Version '" + version + "' is not semantic tag"), Deno.exit(1);
+let version = args._.pop()?.toString();
+console.log("Version: " + version + ", " + typeof version);
+if (version && !version?.match(/^[0-9]+\.[0-9]+\.[0-9]+$/)) console.error("❌ Version '" + version + "' is not a semantic full tag (i.e. X.X.X)"), Deno.exit(1);
 
 // Make sure there are no changes
 const diff = await $`git diff --quiet`.noThrow(1);
@@ -26,23 +36,27 @@ if (fmt.code !== 0) console.error("❌ Code is not formatted"), Deno.exit(1);
 
 // Running Tests
 console.info("🧪 Running Tests");
+console.info("%c\nSTART TESTS\n"+("-".repeat(80)), "color: white");
 const test = await $`deno task test`.noThrow(1);
+console.info("%c"+("=".repeat(80)+"\nEND TESTS\n"), "color: white");
 if (test.code !== 0) console.error("❌ Not all tests passed"), Deno.exit(1);
 
 // If no argument was passed, we will patch the version
 version = version ?? `${major}.${minor}.${patch + 1}`;
 console.info("🆚 version '" + previous + "' -> '" + version + "'");
 
-// Fetch all remote branches to make sure comparison is correct
-console.info("🔽 Fetching from remote");
-await $`git fetch`;
-
-// Releasing
-console.info("🚀 Releasing version '" + version + "' ...");
+// Create notes
 const date = new Date().toISOString().substring(0,10)
 const notes = `### ${version} / ` + date + "\n" + log.map((l) => "- " + l).join("\n");
-await $`gh release create ${version} --title ${version} --notes ${notes}`;
 
-// Pull rebase locally
-console.info("🔽 Pulling from remote");
-await $`git pull --rebase`;
+// Releasing
+if (args.execute) {
+  console.info("🚀 Releasing version '" + version + "' (fetch before, rebase after)...");
+  await $`git fetch`;
+  await $`gh release create ${version} --title ${version} --notes ${notes}`;
+  await $`git pull --rebase`;
+} else {
+  console.info("\n");
+  console.info("%c"+notes, "color: white");
+  console.info("\n" + USAGE);
+}
